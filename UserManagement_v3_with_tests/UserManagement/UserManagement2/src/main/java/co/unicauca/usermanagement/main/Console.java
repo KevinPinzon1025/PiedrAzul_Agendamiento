26838212abe27436344576e02abba0e788318758
Kevin <kevinpinzon@unicauca.edu.co>
2026-03-23 22:57:09 -0500

    Refactor: inyeccion de dependencias, separacion de BD y configuracion .gitignore

--- UserManagement_v3_with_tests/UserManagement/UserManagement2/src/main/java/co/unicauca/usermanagement/main/Console.java
+++ UserManagement_v3_with_tests/UserManagement/UserManagement2/src/main/java/co/unicauca/usermanagement/main/Console.java
@@ -1,37 +1,29 @@
-
 package co.unicauca.usermanagement.main;
 
-import co.unicauca.appointmentmanagement.service.AppointmentServiceImpl;
+import co.unicauca.appointmentmanagement.service.IAppointmentService;
 import co.unicauca.microkernel.core.ReportService;
 import co.unicauca.microkernel.piedaazul.common.entity.AppointmentEntity;
 import co.unicauca.microkernel.piedaazul.common.entity.Report;
 import java.util.List;
 import java.util.Scanner;
 
-/**
- *
- * @author Sam
- */
 public class Console {
-    private AppointmentServiceImpl appointmentService;
+    private IAppointmentService appointmentService;
     private ReportService reportService;
-
     private Scanner scanner;
 
-    public Console(){
-        appointmentService = new AppointmentServiceImpl();
-        reportService = new ReportService();
-        scanner = new Scanner(System.in);
+    public Console(IAppointmentService appointmentService) {
+        this.appointmentService = appointmentService;
+        this.reportService = new ReportService();
+        this.scanner = new Scanner(System.in);
     }
 
     public void start() {
-
         int option;
 
         System.out.println("Generación de reportes");
 
         do {
-
             System.out.println();
             System.out.println("1. Generar reporte de citas");
             System.out.println("2. Salir.");
@@ -44,19 +36,16 @@ public class Console {
                     break;
             }
 
-        } while(option != 2);
+        } while (option != 2);
 
         System.out.println("Aplicación terminada");
     }
 
-    private void handleReportGenerationOption(){
-
+    private void handleReportGenerationOption() {
         List<AppointmentEntity> appointments = appointmentService.getAll();
-        
-        
-        
+
         scanner.nextLine();
-        
+
         System.out.println("Formato al cual se exportaran los datos:");
         System.out.println("1. JSON");
         System.out.println("2. HTML");
@@ -64,7 +53,7 @@ public class Console {
         String option = scanner.nextLine();
         String format = "";
 
-        switch(option){
+        switch (option) {
             case "1":
                 format = "json";
                 break;
@@ -75,24 +64,17 @@ public class Console {
                 System.out.println("Formato inválido");
                 return;
         }
-        
+
         System.out.println("Nombre del archivo: ");
         String fileName = scanner.nextLine();
 
-        //Creamos el objeto que será pasado a la capa de dominio
         Report reportEntity = new Report(appointments, format, fileName);
 
         try {
-
             reportService.generarReporte(reportEntity);
             System.out.println("Archivo creado...");
-
         } catch (Exception exception) {
             System.out.println("No fue posible exportar los datos. " + exception.getMessage());
         }
-
-
-
     }
-
 }
